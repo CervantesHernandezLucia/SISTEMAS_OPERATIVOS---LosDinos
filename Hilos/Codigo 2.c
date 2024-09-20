764369ed0126e3c3b6bbafef321e9a6355434648
Cadabro7 <162276209+Cadabro7@users.noreply.github.com>
2024-09-19 23:18:15 -0600

    Update Codigo 2.c

--- Hilos/Codigo 2.c
+++ Hilos/Codigo 2.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <unistd.h> // Para usar la función sleep()
+#include <unistd.h>
 
-// Función para simular una tarea
 void *tarea(void *arg) {
     int tarea_id = *((int *) arg);
 
     switch (tarea_id) {
         case 1:
             printf("Lavando los platos...\n");
-            // Simulación de la tarea: lavar los platos durante 3 segundos
             sleep(3);
             printf("Platos lavados!\n");
             break;
         case 2:
             printf("Poniendo la lavadora...\n");
-            // Simulación de la tarea: poner la lavadora durante 2 segundos
             sleep(2);
             printf("Lavadora puesta!\n");
             break;
-        // Puedes agregar más casos para simular otras tareas
         default:
             printf("Tarea desconocida.\n");
     }
@@ -31,7 +27,6 @@ int main() {
     pthread_t hilos[2];
     int tareas[] = {1, 2};
 
-    // Crear hilos para cada tarea
     for (int i = 0; i < 2; i++) {
         if (pthread_create(&hilos[i], NULL, tarea, (void *) &tareas[i])) {
             fprintf(stderr, "Error al crear el hilo %d\n", i);
@@ -39,7 +34,6 @@ int main() {
         }
     }
 
-    // Esperar a que ambas tareas terminen
     for (int i = 0; i < 2; i++) {
         if (pthread_join(hilos[i], NULL)) {
             fprintf(stderr, "Error al esperar al hilo %d\n", i);
